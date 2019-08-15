09821a4812e3541594975c04bec094a6cce98d05
Kenton Archer <kenton8654@yahoo.com>
2019-08-14 20:22:56 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,12 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
+    artist = Artist.first
+    # binding.pry
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
