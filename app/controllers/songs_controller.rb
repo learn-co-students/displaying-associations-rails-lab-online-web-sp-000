cd4ff20d28872c59de65727d72bd1a09519235f3
Ronnie Stephens <rstephens@bluemorpho.cc>
2020-01-12 18:36:32 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,13 @@
 class SongsController < ApplicationController
+
   def index
+    @songs = Song.all
   end
 
   def show
-  end
+    @song = Song.find(params[:id])
+    @artist = @song.artist_id
+    end
 
   def new
     @song = Song.new
