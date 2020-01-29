b2048c16cebf367677aa19bc72981d4c20a8c162
Chinedu Arinze <c.s.arinze@gmail.com>
2020-01-29 14:35:19 -0500

    Finshes lab to pass all test.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+heyZooz = Artist.create(name: 'heyZooz')
+jcole = Artist.create(name: 'J Cole')
+kendrick = Artist.create(name: 'Kendrick')
+
+heyZooz.songs.create(title: 'Double Tap')
+jcole.songs.create(title: 'Carolina Freestyle')
+kendrick.songs.create(title: 'D.N.A')
+
