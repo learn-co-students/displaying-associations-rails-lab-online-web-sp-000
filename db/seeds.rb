a787b71aae414b108518bd3aeb1b3fcd6df8e267
ottocedeno <otto@creativespaceclub.com>
2020-05-12 23:12:56 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+met = Artist.create(name: "Metallica")
+met.songs.create(title: "For Whom the Bell Tolls")
+met.songs.create(title: "Blackend")
+met.songs.create(title: "Whiplash")
+
+iron = Artist.create(name: "Iron Maiden")
+iron.songs.create(title: "The Number of The Beast")
+iron.songs.create(title: "The Trooper")
+iron.songs.create(title: "Two Minutes to Midnight")
+
