ec39167187c641683298b02e0692a0c7602f07a9
Juan Carlo Banayo <johncban@gmail.com>
2020-06-20 20:09:41 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,10 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
@@ -48,7 +50,7 @@ class SongsController < ApplicationController
   def song_params
     params.require(:song).permit(:title)
   end
-
+  
   def artist_params
     params.require(:artist).permit(:name)
   end
