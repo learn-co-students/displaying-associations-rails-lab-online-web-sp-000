722182805048e3eb94601988cb5cb091d57a850f
Jivefox <https://github.com/Jivefox>
2020-06-27 23:04:38 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,15 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
     @song = Song.new
+    @artist = @song.artist
   end
 
   def create
