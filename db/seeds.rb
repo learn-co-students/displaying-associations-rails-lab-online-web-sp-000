10b352892a5017cf289c71c03533be180424739b
bruffin1221 <bruffin1221@gmail.com>
2020-07-11 07:52:57 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+m_jackson=Artist.create!(name: "Michael Jackson")
+m_jackson.songs.create!(title:"Beat It")
+m_jackson.songs.create!(title:"Thriller")
+m_jackson.songs.create!(title:"Black or White")
+
+sade=Artist.create!(name: "Sade")
+sade.songs.create(title:"Sweetest Taboo")
+sade.songs.create(title:"Kiss of Life")
+sade.songs.create(title:"Smoot Operator")
