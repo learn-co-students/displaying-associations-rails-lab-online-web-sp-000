b5be65d2de2bf5372b9f3f9e3acaa6fd1d4582f4
connorworthen <connorw220@protonmail.com>
2020-07-13 15:57:11 -0700

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,6 +2,7 @@ class Artist < ActiveRecord::Base
   has_many :songs
 
   def song_count
-    self.song_count
+    songs.count 
   end
+  
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,10 +2,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
-    self.artist.name
+    artist.name
   end
-
-  def song_link
-    self.artist_name + " - " + self.title
-  end
+  
 end
