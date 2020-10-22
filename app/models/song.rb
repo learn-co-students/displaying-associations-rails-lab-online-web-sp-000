791e813f64c5edd1c22e790b7879bbd1306b7c32
blumenthaler <blumenthaleralex@gmail.com>
2020-10-22 14:50:04 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,13 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    if self.artist != nil || self.artist != ""
+      self.artist.name
+    end
+  end
+
+  def formatted_name
+    self.artist_name + " - " + self.title
+  end
 end
