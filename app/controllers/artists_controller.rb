500cc46929ac901126e0bf5115c576657c9f7d3c
github username <github email address>
2020-10-28 22:34:18 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,14 +1,16 @@
 class ArtistsController < ApplicationController
   def index
-  end
-
-  def show
+    @artists = Artist.all
   end
 
   def new
     @artist = Artist.new
   end
 
+  def show
+    @artist = Artist.find(params[:id])
+  end
+  
   def create
     @artist = Artist.new(artist_params)
 
