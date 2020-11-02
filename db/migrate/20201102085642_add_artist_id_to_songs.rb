da44b36d3e79fd06c9d09889a513683a4a7b39d5
Chad Lengefeld <chadlengefeld@gmail.com>
2020-11-02 09:09:57 +0000

    Done.

--- db/migrate/20201102085642_add_artist_id_to_songs.rb
+++ db/migrate/20201102085642_add_artist_id_to_songs.rb
@@ -1,6 +1,6 @@
 class AddArtistIdToSongs < ActiveRecord::Migration[5.0]
-  def add_column
-    add_column :songs do |t|
+  def change
+    change_table :songs do |t|
       t.integer :artist_id
     end
   end
