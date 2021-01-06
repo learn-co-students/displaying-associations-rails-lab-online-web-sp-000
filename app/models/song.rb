22dcc26fa0db30ea9af7d5679f4df4a7fa20cc93
john million <jpmillion@comcast.net>
2021-01-06 18:40:56 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,13 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+
+  def artist_name
+    self.artist.name
+  end
+
+
+  def artist_name_and_song_title
+    self.artist_name + ' - ' + self.title
+  end
 end
