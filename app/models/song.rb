828c43a285b09032b9893da88b05b48641799981
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-05-29 14:36:51 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def artist_name
-    self.artist.name
+  def song_artist=(name)
+    self.artist = Artist.find_or_create_by(name: name)
   end
-  
+
+  def artist_name
+     self.artist ? self.artist.name : nil
+  end
+
 end
